0c21389947895a7db6cd483d96178c1746295c4b
Iiro Alhonen <iiro.alhonen@basemark.com>
2020-03-10 12:17:07 +0200

    Initial Commit

---
+++ Morning Brew/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Morning Brew
+//
+//  Created by Iiro Alhonen on 10.3.2020.
+//  Copyright © 2020 Nifty Tree Studios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

b927bb1e1ebf3eea4fa90da39826c7bf05e36119
Iiro Alhonen <iiro.alhonen@kpsbremen.de>
2020-12-26 12:03:29 +0100

    Removed pods

--- Pods/Target Support Files/Pods-Bean Juice-Bean JuiceUITests/Pods-Bean Juice-Bean JuiceUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Bean_Juice_Bean_JuiceUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Bean_Juice_Bean_JuiceUITests
-@end
--- Pods/Target Support Files/Pods-Bean JuiceTests/Pods-Bean JuiceTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Bean_JuiceTests : NSObject
-@end
-@implementation PodsDummy_Pods_Bean_JuiceTests
-@end

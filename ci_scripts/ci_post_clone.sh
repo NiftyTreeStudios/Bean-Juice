6ec338254170b482a807cdfaae17d4bb5a914192
iiroalhonen <iiro.alhonen@kpsbremen.de>
2021-11-25 15:26:42 +0100

    ci script added

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Bean Juice
+#
+#  Created by Iiro Alhonen on 25.11.21.
+#  Copyright © 2021 Nifty Tree Studios. All rights reserved.
+
+# Install CocoaPods using Homebrew.
+brew install cocoapods
+		
+# Install dependencies you manage with CocoaPods.
+pod install

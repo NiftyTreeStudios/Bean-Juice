6deadb079879d6cecd53cf1e08c34be99ee0be3c
Iiro Alhonen <iiro.alhonen@sanoma.com>
2022-02-12 09:54:30 +0200

    Cleaning project structure to enable modularization.

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Bean-Juice",
-            targets: ["Bean-Juice"]),
+            name: "Models",
+            targets: ["Models"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Bean-Juice",
+            name: "Models",
             dependencies: []),
-        .testTarget(
-            name: "Bean-JuiceTests",
-            dependencies: ["Bean-Juice"]),
     ]
 )

2af2080c21f9ce9e846421ac0e827d5bff2898dc
Iiro Alhonen <iiro.alhonen@sanoma.com>
2022-02-12 09:32:58 +0200

    Remove CocoaPods and init spm.

---
+++ Sources/Bean-Juice/Bean_Juice.swift
@@ -0,0 +1,6 @@
+public struct Bean_Juice {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/Bean-JuiceTests/Bean_JuiceTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Bean_Juice
+
+final class Bean_JuiceTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Bean_Juice().text, "Hello, World!")
+    }
+}

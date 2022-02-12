5d4db9769d9ce63baeb46e6c1433029d8c6f7540
Iiro Alhonen <iiro.alhonen@sanoma.com>
2022-02-12 10:11:28 +0200

    Extract CustomColor.

--- Sources/Models/Models.swift
+++ Sources/Models/Models.swift
@@ -1,3 +1,5 @@
+import SwiftUI
+
 /// Model representing cup size.
 public struct CupSize {
     public let name: String
@@ -9,5 +11,19 @@ public struct CupSize {
     ) {
         self.name = name
         self.sizeMl = sizeMl
+    }
+}
+
+/// Custom color struct
+public struct CustomColor {
+    public let name: String
+    public let color: Color
+    
+    public init(
+        name: String,
+        color: Color
+    ) {
+        self.name = name
+        self.color = color
     }
 }

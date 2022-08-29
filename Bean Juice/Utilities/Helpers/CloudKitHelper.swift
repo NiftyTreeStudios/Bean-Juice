a2cc6a97025b342e60b3054f59d02bf4f2a2bb3a
Iiro Alhonen <iiro.alhonen@sanoma.com>
2022-08-29 11:11:55 +0300

    Fix indentation.

--- Bean Juice/Utilities/Helpers/CloudKitHelper.swift
+++ Bean Juice/Utilities/Helpers/CloudKitHelper.swift
@@ -9,29 +9,29 @@
 import CloudKit
 
 struct CloudKitHelper {
-  
-  static let kCKIdentifier = "iCloud.com.niftytreestudios.BeanJuice"
-  
-  static func getCafes(completed: @escaping (Result<[Cafe], Error>) -> Void) {
-    let query = CKQuery(
-      recordType: RecordType.cafe,
-      predicate: NSPredicate(value: true)
-    )
-    Task {
-      do {
-        let matchResults = try await CKContainer(identifier: kCKIdentifier)
-          .publicCloudDatabase // Default would be CKContainer.default().public...
-          .records(matching:query)
-          .matchResults
-        let cafes = matchResults
-          .map { $0.1 }
-          .compactMap { try? $0.get() }
-          .compactMap(Cafe.init)
-        completed(.success(cafes))
-      }
-      catch {
-        completed(.failure(error))
-      }
+    
+    static let kCKIdentifier = "iCloud.com.niftytreestudios.BeanJuice"
+    
+    static func getCafes(completed: @escaping (Result<[Cafe], Error>) -> Void) {
+        let query = CKQuery(
+            recordType: RecordType.cafe,
+            predicate: NSPredicate(value: true)
+        )
+        Task {
+            do {
+                let matchResults = try await CKContainer(identifier: kCKIdentifier)
+                    .publicCloudDatabase // Default would be CKContainer.default().public...
+                    .records(matching:query)
+                    .matchResults
+                let cafes = matchResults
+                    .map { $0.1 }
+                    .compactMap { try? $0.get() }
+                    .compactMap(Cafe.init)
+                completed(.success(cafes))
+            }
+            catch {
+                completed(.failure(error))
+            }
+        }
     }
-  }
 }
